829af13177ababfcf9eed79e90c1da9cde7ae9f2
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-06-07 14:06:08 +0700

    estimate_fama_macbeth With (In) R Software

--- estimate_fama_macbeth/estimate_fama_macbeth.R
+++ estimate_fama_macbeth/estimate_fama_macbeth.R
@@ -6,7 +6,7 @@ install.packages("tidyfinance")
 
 library("tidyfinance")
 # Estimate Fama-MacBeth Regressions Use estimate_fama_macbeth (tidyfinance) With (In) R Software
-estimate_fama_macbeth_ = read.csv("https://raw.githubusercontent.com/timbulwidodostp/estimate_fama_macbeth_/main/estimate_fama_macbeth_/estimate_fama_macbeth_.csv",sep = ";")
+estimate_fama_macbeth_ = read.csv("https://raw.githubusercontent.com/timbulwidodostp/estimate_fama_macbeth/main/estimate_fama_macbeth/estimate_fama_macbeth.csv",sep = ";")
 estimate_fama_macbeth <- estimate_fama_macbeth(estimate_fama_macbeth_, "ret_excess ~ beta + bm + log_mktcap")
 estimate_fama_macbeth_ <- estimate_fama_macbeth(estimate_fama_macbeth_, "ret_excess ~ beta + bm + log_mktcap", detail = TRUE)
 estimate_fama_macbeth
